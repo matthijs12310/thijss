0bb76160d32eb7e7962976b3a15ab1a894d2f25c
matthijs12310 <bobstoffer1596@gmail.com>
2021-06-27 10:24:05 +0200

    f

--- cmd/cmd.js
+++ cmd/cmd.js
@@ -1,14 +1,20 @@
 function showIt() {
     document.getElementById("guest").style.visibility = "visible";
   }
-setTimeout("showIt()", 1000); // after 1 sec
+setTimeout("showIt()", 1000);
 function showIt2() {
     document.getElementById("demo").style.visibility = "visible";
   }
-setTimeout("showIt2()", 1500); // after 5 secs
+setTimeout("showIt2()", 1500);
+function showIt3() {
+  document.getElementById("matthijs").style.visibility = "visible";
+}
+setTimeout("showIt3()", 5000)
 
 var i = 0;
+var i2 = 0;
 var txt = "./matthijs.tk"; /* The text */
+var txt2 = "Loading... OK"
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
@@ -18,4 +24,12 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+function typeWriter2() {
+  if (i2 < txt.length) {
+    document.getElementById("loading").innerHTML += txt2.charAt(i2);
+    i2++;
+    setTimeout(typeWriter2, speed);
+  }
+}
+setTimeout("typeWriter2()", 4000)
 setTimeout("typeWriter()", 2000);
