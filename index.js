2474ade460cf40efd38f7fea083f14fd10e0c4c0
matthijs12310 <bobstoffer1596@gmail.com>
2021-10-09 22:59:14 +0200

    h

--- index.js
+++ index.js
@@ -1,44 +1,32 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const { exec } = require("child_process");
 const app = express();
 const port = process.env.PORT || 8080;
+const AWS = require('aws-sdk');
 
 
-exec("cd s3cmd-master/ && chmod +x ./s3cmd && python -m ensurepip --upgrade --user && pip install python-dateutil && pip install python-dateutil && ./s3cmd ls s3://files.matthijs.tk", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-       
-    }
-    console.log(`${stdout}`)
+AWS.config.update({ accessKeyId: '17Y4ZE73T93MJ1YTVPB9', secretAccessKey: 'dPq3TiIx7uR4sg8Jo0uKKMNUNBH7Q1mpmzJcYNZl' });
 
+const s3 = new AWS.S3({ endpoint: 'cellar-c2.services.clever-cloud.com' });
+var params = {
+    Bucket: "files.matthijs.tk"
+};
 
-    console.log("fdgshg")
-    });
-
-
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
-  });
-app.listen(port);
-app.get('/click', function(req, res) {
-
-    exec("ls", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    res.send(`${stdout}`)
-    });
-
-
 });
+
+app.listen(port);
+
+app.get('/click', function (req, res) {
+    s3.listObjectsV2(params, function (err, s3res) {
+        if (err) {
+            return;
+        }
+
+        res.json(s3res.Contents);
+    });
+});
+
 console.log('Server started at http://localhost:' + port);
