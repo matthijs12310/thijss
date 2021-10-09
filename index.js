bc912066dc6f5ee04e8f7b1b4c3b1f8e85755247
matthijs12310 <bobstoffer1596@gmail.com>
2021-10-09 20:55:37 +0200

    g

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { exec } = require("child_process");
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+exec("apt-get update && apt install python && apt install python-dateutil && cp .s3cfg /root/.s3cfg", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+       
+    }
+    console.log(`${stdout}`)
+
+
+    console.log("fdgshg")
+    });
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+app.listen(port);
+app.get('/click', function(req, res) {
+
+    exec("ls", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    res.send(`${stdout}`)
+    });
+
+
+});
+console.log('Server started at http://localhost:' + port);
