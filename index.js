188cbba757469dc92b61174cea53f817afab95ed
matthijs12310 <bobstoffer1596@gmail.com>
2021-10-10 13:34:19 +0200

    j

--- index.js
+++ index.js
@@ -4,19 +4,45 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 const AWS = require('aws-sdk');
-
+const formidable = require('formidable');
+const mv = require('mv');
+const { IncomingForm } = require('formidable');
+const { json } = require('express');
+const { delimiter } = require('path');
 
 AWS.config.update({ accessKeyId: '17Y4ZE73T93MJ1YTVPB9', secretAccessKey: 'dPq3TiIx7uR4sg8Jo0uKKMNUNBH7Q1mpmzJcYNZl' });
+app.use(express.static(__dirname + '/'));
 
 const s3 = new AWS.S3({ endpoint: 'cellar-c2.services.clever-cloud.com' });
 var params = {
-    Bucket: "files.matthijs.tk"
+    Bucket: "files.matthijs.tk",
 };
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
-
+app.post('/api/upload', function(req, res) {
+    const form = new formidable.IncomingForm();
+ 
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    var oldpath = files.someExpressFiles.path
+    var newpath = files.someExpressFiles.name
+    var object = fs.readFileSync(oldpath)
+    var params2 = {
+        Body: object,
+        Bucket: "files.matthijs.tk",
+        Key: newpath
+    };
+    s3.putObject(params2, function(err, data){
+        console.log("File " + newpath + " Uploaded!")
+    })
+  });
+  res.redirect('/')
+});
 app.listen(port);
 
 app.get('/click', function (req, res) {
@@ -24,7 +50,6 @@ app.get('/click', function (req, res) {
         if (err) {
             return;
         }
-
         res.json(s3res.Contents);
     });
 });
